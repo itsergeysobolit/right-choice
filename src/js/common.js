af04d307064e95a2d250f625ebfa9658d29838a8
Sergey <scandy_boss@ukr.net>
2018-11-30 17:45:07 +0200

    finish tabs menu

--- src/js/common.js
+++ src/js/common.js
@@ -1,7 +1,20 @@
 $(function() {
 
+
+	//menu in icons
 	$('#icon-menu').click(function() {
 		 var menuItems = $('#icon-menu-items');
+
+    	if(menuItems.is(':visible'))
+        	menuItems.slideUp('slow');
+    	else
+        	menuItems.slideDown('slow');
+		})
+
+
+	//menu in tabs
+	$('#tabs-menu').click(function() {
+		 var menuItems = $('#tabs-menu-content');
 
     	if(menuItems.is(':visible'))
         	menuItems.slideUp('slow');
